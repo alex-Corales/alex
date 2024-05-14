83722025cbb289141dcd617da6481d29d02bf1a5
Alex Nahuel Corales <alexcorales21@gmail.com>
2024-05-14 19:29:23 -0300

    commit

---
+++ js/main.js
@@ -0,0 +1,23 @@
+let currentIndex = 0;
+const slides = document.querySelectorAll('.slider img');
+const totalSlides = slides.length;
+
+function showSlide(index) {
+  slides.forEach((slide) => {
+    slide.style.display = 'none';
+  });
+  slides[index].style.display = 'block';
+}
+
+function nextSlide() {
+  currentIndex = (currentIndex + 1) % totalSlides;
+  showSlide(currentIndex);
+}
+
+function prevSlide() {
+  currentIndex = (currentIndex - 1 + totalSlides) % totalSlides;
+  showSlide(currentIndex);
+}
+
+// Mostrar el primer slide al cargar la página
+showSlide(currentIndex);
